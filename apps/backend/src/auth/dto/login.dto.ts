b283d9c77c3af531df4ebd2f8e732f6616aa2c87
Jefferson <jefferson1999rosales@gmail.com>
2025-10-23 15:27:14 -0500

    EC-3 dev(auth): implementar signup y login con JWT y bcrypt (#3)

--- apps/backend/src/auth/dto/login.dto.ts
+++ apps/backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class LoginDto {
-  username!: string;
-  password!: string;
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
 }
