c68f3d0e8bc18740e8fa4460958ee02280a54dcf
JuanAldairRamirezMendez <u20201597@utp.edu.pe>
2025-12-12 00:21:58 -0500

    Add favicon.ico route to prevent 404 errors

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('favicon.ico')
+  @ApiExcludeEndpoint()
+  getFavicon() {
+    return '';
+  }
 
   @Get()
   @Redirect('/api/docs')
